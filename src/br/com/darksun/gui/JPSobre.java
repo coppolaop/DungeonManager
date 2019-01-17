6feba52f794b31ab5e9978ffe6a250f56d539f8c
vitor-abel <abelabelao@hotmail.com.br>
2019-01-16 23:49:46 -0200

    Closes #54: Incluindo novos desenvolvedores na lista

--- src/br/com/darksun/gui/JPSobre.java
+++ src/br/com/darksun/gui/JPSobre.java
@@ -23,9 +23,8 @@ public class JPSobre extends JPPadrao
 		JLabel labelImg = new JLabel( "" );
 		Integer imgSize = 200;
 		labelImg.setBounds( ( width - imgSize ) / 2, 50, imgSize, imgSize );
-		ImageIcon logoApp = new ImageIcon( getClass( ).getClassLoader( ).getResource( frame.getIconPath( ) ) );
-		logoApp = new ImageIcon(logoApp.getImage( ).getScaledInstance(labelImg.getWidth(),labelImg.getHeight(), logoApp.getImage( ).SCALE_DEFAULT));
-		labelImg.setIcon(logoApp);
+		Image logoApp = Toolkit.getDefaultToolkit( ).getImage( frame.getIconPath( ) );
+		labelImg.setIcon(new ImageIcon(logoApp.getScaledInstance(labelImg.getWidth(),labelImg.getHeight(), logoApp.SCALE_DEFAULT)));
 
 		JLabel labelSystem = new JLabel( "Dungeon Manager" );
 		labelSystem.setBounds( 0, height / 4 + 50, width, 70 );
@@ -34,26 +33,56 @@ public class JPSobre extends JPPadrao
 				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 50 ) );
 		labelSystem.setHorizontalAlignment( SwingConstants.CENTER );
 
-		JLabel labelCreator = new JLabel( "Desenvolvedor: Marcos Alves Gonçalves Junior (Coppola)" );
+		JLabel labelCreator = new JLabel( "Criador: Marcos \"Coppola\" Gonçalves (coppolaop)" );
 		labelCreator.setBounds( 0, height / 4 + 100, width, 70 );
 		labelCreator.setForeground( Color.CYAN );
 		labelCreator
 				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 30 ) );
 		labelCreator.setHorizontalAlignment( SwingConstants.CENTER );
-
+		
 		JLabel labelGithub = new JLabel( "Repositório Online: github.com/coppolaop/DungeonManager" );
 		labelGithub.setBounds( 0, height / 4 + 150, width, 70 );
 		labelGithub.setForeground( Color.CYAN );
 		labelGithub
 				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 30 ) );
 		labelGithub.setHorizontalAlignment( SwingConstants.CENTER );
+		
+		JLabel labelDeveloper1 = new JLabel( "Desenvolvedores:");
+		labelDeveloper1.setBounds( 0, height / 4 + 250, width, 70 );
+		labelDeveloper1.setForeground( Color.CYAN );
+		labelDeveloper1
+				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 30 ) );
+		labelDeveloper1.setHorizontalAlignment( SwingConstants.CENTER );
+		
+		JLabel labelDeveloper2 = new JLabel( "Marcos \"Coppola\" Gonçalves (coppolaop)");
+		labelDeveloper2.setBounds( 0, height / 4 + 300, width, 70 );
+		labelDeveloper2.setForeground( Color.CYAN );
+		labelDeveloper2
+				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 30 ) );
+		labelDeveloper2.setHorizontalAlignment( SwingConstants.CENTER );
+		
+		JLabel labelDeveloper3 = new JLabel( "Vitor Abel (vitor-abel)");
+		labelDeveloper3.setBounds( 0, height / 4 + 350, width, 70 );
+		labelDeveloper3.setForeground( Color.CYAN );
+		labelDeveloper3
+				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 30 ) );
+		labelDeveloper3.setHorizontalAlignment( SwingConstants.CENTER );
+		
+		JLabel labelDeveloper4 = new JLabel( "Igor Huayck (flaigor)");
+		labelDeveloper4.setBounds( 0, height / 4 + 400, width, 70 );
+		labelDeveloper4.setForeground( Color.CYAN );
+		labelDeveloper4
+				.setFont( new Font( labelSystem.getFont( ).getFontName( ), labelSystem.getFont( ).getStyle( ), 30 ) );
+		labelDeveloper4.setHorizontalAlignment( SwingConstants.CENTER );
 
 		add( labelSystem );
 		add( labelCreator );
 		add( labelGithub );
+		add( labelDeveloper1 );
+		add( labelDeveloper2 );
+		add( labelDeveloper3 );
+		add( labelDeveloper4 );
 		add( labelImg );
-		
-		frame.repaint( );
 		
 		frame.addComponentListener( new ComponentAdapter( )
 		{
@@ -68,6 +97,10 @@ public class JPSobre extends JPPadrao
 				labelSystem.setBounds( 0, height / 4 + 50, width, 70 );
 				labelCreator.setBounds( 0, height / 4 + 100, width, 70 );
 				labelGithub.setBounds( 0, height / 4 + 150, width, 70 );
+				labelDeveloper1.setBounds( 0, height / 4 + 250, width, 70 );
+				labelDeveloper2.setBounds( 0, height / 4 + 300, width, 70 );
+				labelDeveloper3.setBounds( 0, height / 4 + 350, width, 70 );
+				labelDeveloper4.setBounds( 0, height / 4 + 400, width, 70 );
 			}
 		});
 	}
